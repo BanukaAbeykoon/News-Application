f11cd27f766560e024e607026745e014cc71f492
BanukaAbeykoon <it20203658@my.sliit.lk>
2023-10-24 22:30:55 +0530

    last

---
+++ src/app/Components/Register.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+export default function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+    // Implement your registration logic here
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleRegister}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
